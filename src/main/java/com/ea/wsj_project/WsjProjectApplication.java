b7fd4c5a900a4bd2c136a6bedf0d8c2d2c73d7c0
EricNorrwing <eric.norrwing@gmail.com>
2024-09-06 14:41:12 +0200

    First commit

---
+++ src/main/java/com/ea/wsj_project/WsjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ea.wsj_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsjProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsjProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ea/wsj_project/WsjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.wsj_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
